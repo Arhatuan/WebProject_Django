c3f1a193f7af19cb9bc714b6280d68d7e7511626
Arhatuan <coudertalexandre.s@gmail.com>
2026-04-08 19:49:01 +0200

    test change 2

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # }
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', default=f'sqlite:///{BASE_DIR}/db.sqlite3'),
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3',
         conn_max_age=600
     )
 }
